0beb1bf405c41bd758a796b9bc3ceafbe5df39d2
Scherbin <97916851+Scherbin@users.noreply.github.com>
2022-09-10 19:27:24 +0300

    Add_AHUD

--- Source/Shooter/Private/STUGameModeBase.cpp
+++ Source/Shooter/Private/STUGameModeBase.cpp
@@ -4,9 +4,11 @@
 #include "STUGameModeBase.h"
 #include "Player/STUBase_Character.h"
 #include "Player/STUPlayerController.h"
+#include "UI/STUGameHUD.h"
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
 	DefaultPawnClass = ASTUBase_Character::StaticClass();
 	PlayerControllerClass = ASTUPlayerController::StaticClass();
+	HUDClass = ASTUGameHUD::StaticClass();
 }
---
+++ Source/Shooter/Public/UI/STUGameHUD.h
@@ -0,0 +1,24 @@
+// Shooter, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "STUGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API ASTUGameHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+    virtual void DrawHUD() override;
+
+private:
+	void DrawCrossHair();
+
+
+};
