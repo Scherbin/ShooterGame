17587157ad0a6199183ab14aef37555645db0851
Scherbin <97916851+Scherbin@users.noreply.github.com>
2023-01-23 13:54:45 +0200

    AddSoundMenu

--- Source/Shooter/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/Shooter/Private/Menu/UI/STUMenuWidget.cpp
@@ -8,6 +8,7 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Components/HorizontalBox.h"
 #include "Menu/UI/STULevelItemWidget.h"
+#include "Sound/SoundCue.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogSTUMenuWidget, All, All);
 
@@ -80,6 +81,7 @@ void USTUMenuWidget::OnStartGame()
 {
 	PlayAnimation(HideAnimation);
 	
+	UGameplayStatics::PlaySound2D(GetWorld(), StartGameSound);
 }
 
 void USTUMenuWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* Animation)
--- Source/Shooter/Private/UI/STUBaseWidget.cpp
+++ Source/Shooter/Private/UI/STUBaseWidget.cpp
@@ -2,8 +2,12 @@
 
 
 #include "UI/STUBaseWidget.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 
 void USTUBaseWidget::Show()
 {
 	PlayAnimation(ShowAnimation);
+
+	UGameplayStatics::PlaySound2D(GetWorld(), OpenSound);
 }
